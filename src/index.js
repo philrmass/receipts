5cfd344bbf2926e2dbabc5ad1930e7f64a7837f3
Phil Mass <philrmass@gmail.com>
2023-06-08 20:56:01 -0700

    add deploy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './style';
 
 export default function App() {
-	return (
-		<div>
-			<h1>Hello, World!</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
 }

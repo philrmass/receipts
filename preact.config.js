6a3530ef3925d4476ed778c49b1da942b5c47341
Phil Mass <philrmass@gmail.com>
2026-02-17 22:42:52 -0800

    update config

--- preact.config.js
+++ preact.config.js
@@ -1,12 +1,4 @@
-module.exports = (config, env, helpers) => {
+module.exports = (config) => {
   const isDev = Boolean(process.env.DEV);
   config.output.publicPath = isDev ? '' : '/receipts/';
-  console.log(`Using ${isDev ? 'dev' : 'prod'} publicPath='${config.output.publicPath}'`);
-
-  // use the public path in your app as 'process.env.PUBLIC_PATH'
-  config.plugins.push(
-    new helpers.webpack.DefinePlugin({
-      'process.env.PUBLIC_PATH': JSON.stringify(config.output.publicPath || '/'),
-    }),
-  );
 };
